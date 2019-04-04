7a85ec8166abd4ef39a008343545e07f55c94621
Kenny Scharm <kscharm3@gatech.edu>
2019-04-03 22:00:58 -0400

    toString method

--- flask/static/messenger-3.py
+++ flask/static/messenger-3.py
@@ -6,7 +6,6 @@ from googlevoice import Voice
 
 
 class Messenger():
-
     def __init__(self):
         print("Messenger Running...")
         self.debug = True
@@ -32,28 +31,23 @@ class Messenger():
 
     def run(self):
         i = 0
-
         voice = Voice()
         voice.login(self.username, self.password)
-
         while True:
             i += 1
-
             voice.sms()
-
             for msg in self.extractsms(voice.sms.html):
                 if msg:
-
                     if self.debug:
                         print(msg)
-
                     caller = msg["from"].replace("+", "").replace(":", "")
                     print(msg["time"] + "," + caller + "," + msg["text"], file=open("data.csv", "a"))
-                    reply = str(self.patientDatabase.getPatient(msg["text"]))
-                    if reply == None:
-                        reply = "error"
-                    print(msg["time"] + "," + "17408720211" + "," + reply, file=open("data.csv", "a"))
-                    voice.send_sms(caller, reply)
+                    reply = self.patientDatabase.getPatient(msg["text"])
+                    replyStr = self.toString(reply)
+                    if replyStr == None:
+                        replyStr = "error"
+                    print(msg["time"] + "," + "17408720211" + "," + replyStr, file=open("data.csv", "a"))
+                    voice.send_sms(caller, replyStr)
 
                     for message in voice.sms().messages:
                         if message.isRead:
@@ -62,6 +56,17 @@ class Messenger():
             if self.debug:
                 print('Idle Counter: ' + str(i))
             time.sleep(self.timer)
+    def toString(self, obj):
+        reply = ''
+        for key, val in obj.__dict__.items():
+            if type(val) is str:
+                reply += key + ": " + val + '\n'
+            if key == 'admissions':
+                admissions = ''
+                for admission in key:
+                    admissions += 'code: ' + admission[0] + ', ' admission[1] + '\n'
+                reply += admissions
+        return reply
 
 m = Messenger()
 m.run()
